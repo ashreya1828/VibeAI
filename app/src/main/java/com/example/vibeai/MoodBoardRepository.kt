9f03c6894405b89389c5c1f2b69923f6f6beb3b1
ashreya1828 <ashreya1828@gmail.com>
2025-11-30 13:38:56 +0000

    Connecting room data base

--- app/src/main/java/com/example/vibeai/MoodBoardRepository.kt
+++ app/src/main/java/com/example/vibeai/MoodBoardRepository.kt
@@ -3,8 +3,8 @@ package com.example.vibeai
 import androidx.compose.runtime.mutableStateListOf
 
 object MoodBoardRepository {
-    // Shared list of mood boards seen by Home & CreateMood
-    val boards = mutableStateListOf<MoodBoard>().apply {
-        addAll(sampleMoodBoards()) // start with your sample data
-    }
+
+
+    val boards = mutableStateListOf<MoodBoard>()
+
 }
